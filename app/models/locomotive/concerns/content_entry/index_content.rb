201dca8c6e0671f1c6a1f508273a40f7409fc581
Pete R <peachananr@gmail.com>
2020-03-24 15:55:54 +0700

    truncate search

--- app/models/locomotive/concerns/content_entry/index_content.rb
+++ app/models/locomotive/concerns/content_entry/index_content.rb
@@ -54,7 +54,7 @@ module Locomotive
               end
 
               text_only = sanitize_search_content(html.inner_html)
-              text_only.downcase.chomp.gsub(/[^0-9A-Za-z ]/, ' ').split(" ").uniq.select{|w| w.length >= 3}.join(" ")
+              truncate_desc(text_only.downcase.chomp.gsub(/[^0-9A-Za-z ]/, ' ').split(" ").uniq.select{|w| w.length >= 3}.join(" "), 40000)
             else
               next
             end
