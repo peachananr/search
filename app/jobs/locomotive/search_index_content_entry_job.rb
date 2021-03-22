289ee19b5ac6c3bb00390320ca1441e6fdd5a2f8
Pete R <peachananr@gmail.com>
2021-03-22 14:46:53 +0700

    Revert

--- app/jobs/locomotive/search_index_content_entry_job.rb
+++ app/jobs/locomotive/search_index_content_entry_job.rb
@@ -9,26 +9,7 @@ module Locomotive
         if entry._type == "Locomotive::ContentEntry5adf77af6eabcc00190b75b6" or entry._type == "Locomotive::ContentEntry5ae2fcb93e788b000b95ee64" or entry._type == "Locomotive::ContentEntry5afe6305a6c15b186b7d1943"
           index_important_post(entry.site, entry, locale)
         end
-
-
-
-
-        # Run Clear Cache Rake On Save Using Rake Tasks from Engine
-        puts "xxxx #{entry._slug}"
-
-        if entry._type == "Locomotive::ContentEntry5adf77af6eabcc00190b75b6" or entry._type == "Locomotive::ContentEntry5ae2fcb93e788b000b95ee64" or entry._type == "Locomotive::ContentEntry5afe6305a6c15b186b7d1943"
-          if entry._type == "Locomotive::ContentEntry5adf77af6eabcc00190b75b6"
-            url = "/posts/#{entry._slug}"
-          end
-          if entry._type == "Locomotive::ContentEntry5ae2fcb93e788b000b95ee64"
-            url = "/destinations/#{entry._slug}"
-          end
-          if entry._type == "Locomotive::ContentEntry5afe6305a6c15b186b7d1943"
-            url = "/videos/#{entry._slug}"
-          end
-          require 'rake'
-          Rake::Task["admin:redis_cc"].invoke("#{url}")
-        end
+        
         #else
         #  index_content_entry(entry.site, entry, locale)
         #end
