15f915dce89be92d1619775355ec8e77ae62740c
Pete R <peachananr@gmail.com>
2021-03-22 14:16:33 +0700

    Fix more

--- app/jobs/locomotive/search_index_content_entry_job.rb
+++ app/jobs/locomotive/search_index_content_entry_job.rb
@@ -4,11 +4,19 @@ module Locomotive
     def perform(entry_id, locale)
       ::Mongoid::Fields::I18n.with_locale(locale) do
         entry = Locomotive::ContentEntry.find(entry_id)
-        
+
         #if entry.site == "5adf778b6eabcc00190b75b1" #dev"5ae3ea9872822817a85b0d64"
         if entry._type == "Locomotive::ContentEntry5adf77af6eabcc00190b75b6" or entry._type == "Locomotive::ContentEntry5ae2fcb93e788b000b95ee64" or entry._type == "Locomotive::ContentEntry5afe6305a6c15b186b7d1943"
           index_important_post(entry.site, entry, locale)
         end
+
+
+
+
+        # Run Clear Cache Rake On Save Using Rake Tasks from Engine
+        puts "xxxx #{entry}"
+        #Rake::Task["admin:redis_cc"].invoke('')
+
         #else
         #  index_content_entry(entry.site, entry, locale)
         #end
