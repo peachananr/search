54be1eaa094257b322f6c9f4af1089e9ee091822
Pete Rojwongsuriya <peterojwongsuriya@192.168.1.38>
2023-12-18 14:03:54 +0700

    Update

--- app/jobs/locomotive/search_index_content_entry_job.rb
+++ app/jobs/locomotive/search_index_content_entry_job.rb
@@ -4,12 +4,11 @@ module Locomotive
     def perform(entry_id, locale)
       ::Mongoid::Fields::I18n.with_locale(locale) do
         entry = Locomotive::ContentEntry.find(entry_id)
-        puts "#{entry.site._id} xxxxxxxx"
-        if entry.site == "5adf778b6eabcc00190b75b1"
+        if entry.site._id == "5adf778b6eabcc00190b75b1"
           if entry._type == "Locomotive::ContentEntry5adf77af6eabcc00190b75b6" or entry._type == "Locomotive::ContentEntry5ae2fcb93e788b000b95ee64" or entry._type == "Locomotive::ContentEntry5afe6305a6c15b186b7d1943"
             index_bucketlistly_post(entry.site, entry, locale)
           end
-        elsif entry.site == "639b26afb83a540004858288"
+        elsif entry.site._id == "639b26afb83a540004858288"
           if entry._type == "Locomotive::ContentEntry639b2d4bb83a54000485828d"
             index_mintsmeals_post(entry.site, entry, locale)
           end
